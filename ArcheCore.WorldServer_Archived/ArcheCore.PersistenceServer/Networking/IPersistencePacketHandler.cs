e4d4bf9c7bb02135e1177b50d15ef7478746062a
Thepipsqueak <98383696+Thepipsqueak62@users.noreply.github.com>
2026-09-11 13:17:57 -0400

    Removed ArcheCore.Worldserver_Archive
    Basically my first versions of the MMORPG in unity.

--- ArcheCore.WorldServer_Archived/ArcheCore.PersistenceServer/Networking/IPersistencePacketHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using MessagePack;
-
-namespace ArcheCore.WorldServer.PersistenceServer.Networking
-{
-    public interface IPersistencePacketHandler
-    {
-        void Handle(PersistencePacket persistencePacket);
-    }
-}
