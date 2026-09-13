1b9eedd2d52792e892cac9bdfd7452b6acded5b0
Thepipsqueak <98383696+Thepipsqueak62@users.noreply.github.com>
2026-09-13 01:01:39 -0400

    added test button to send itemrequest

---
+++ Assets/ArcheCore.Client/Scripts/testing/TItemRequestTestButton.cs
@@ -0,0 +1,24 @@
+﻿using ArcheCore.Client.Networking;
+using ArcheCore.Client.Networking.C2WSenders;
+using UnityEngine;
+
+namespace ArcheCore.Client.testing
+{
+    public class TItemRequestTestButton : MonoBehaviour
+    {
+        [SerializeField] private int testItemId = 1;
+
+
+        public void OnRequestItemButtonClicked()
+        {
+            if (ClientNetwork.Instance == null || ClientNetwork.Instance.ServerPeer == null)
+            {
+                Debug.LogWarning("Not Connected to WorldServer");
+                return;
+            }
+
+            C2WItemRequestDataPacketSender.Send(ClientNetwork.Instance.ServerPeer, testItemId);
+        }
+        
+    }
+}
